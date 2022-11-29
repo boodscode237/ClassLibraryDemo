098391394ec838d48f8c3bb2c505a300633041eb
boodscode237 <boodscode@gmail.com>
2022-11-30 04:03:10 +0500

    Add project files.

---
+++ ConsoleUI06/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoLibrary;
+
+namespace ConsoleUI06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonModel person = new PersonModel();
+            Console.ReadLine();
+        }
+    }
+}
